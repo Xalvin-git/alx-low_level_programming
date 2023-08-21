3d9478da8fb9993205cfbec5aeaf3440fa18ddd1
Pelumi <fishopelumialvin@gmail.com>
2023-08-21 06:05:24 -0700

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/** 
+*main - determine if a number is positive negative or zero.
+(*0 : is the number to be checked 
+* Return: o on success
+*/
+void positive_or_negative(int i)
+{
+if (i < 0)
+{
+printf("%d is %s\n", 1, "negative");
+}
+else if (i > 0)
+{
+printf("%d is %s\n", 1, "positive");
+}
+else
+{
+printf("%d is %s\n", 1, "zero");
+}
+return;
+}
